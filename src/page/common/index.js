82b557db280377dbbc593956904d65ddb5cefc94
BaiWeiJieKu <2534964002@qq.com>
2019-09-12 21:39:59 +0800

    通用样式，通用方法

---
+++ src/page/common/index.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Rosen
+* @Date:   2017-05-09 16:48:55
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-05-17 21:20:30
+*/
+
+// "严格模式"，这种模式使得Javascript在更严格的条件下运行
+'use strict';
+// 引入layout.css
+require('./layout.css');
+//引入图标
+require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
